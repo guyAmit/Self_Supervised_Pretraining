d6fb10d1f252a1f656cd60337b9f711a1e7baf35
guy <guy5@post.bgu.ac.il>
2021-10-01 19:31:41 +0300

    README and Args FIX

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# Self_Supervised_Pretraining
-A repository containing methods to pre train DNN for image task using self supervised learning
+# Self Supervised Pretraining
+A repository containing methods to pre train DNN for image tasks
+using self supervised learning.
+
+## Self Supervised Methods:
+1. [SimCLR][https://arxiv.org/abs/2002.05709]
+2. [InPainting][https://arxiv.org/pdf/1604.07379.pdf]
+3.
+
+## Run Examples
+1. Train Resnet18 using SimCLR on stl10 dataset with 3 views
+```
+python main.py -type SimCLR --arch Resnet18 --dataset stl10 --n_views 3
+```
+2. Train Resnet43 using InPainting on cifar10 with mask size of 8
+```
+python main.py -type InPainting --arch Resnet34 --dataset cifar10 --mask_size 8
+```
+
+## Softwere Requirments
+```
+numpy=1.19.2=py38h54aff64_0
+numpy-base=1.19.2=py38hfa32c7d_0
+pytorch=1.8.1=py3.8_cuda11.1_cudnn8.0.5_0
+scikit-image=0.17.2=py38hdf5156a_0
+scipy=1.4.1=py38h0b6359f_0
+torchvision=0.9.1=py38_cu111
+
+```
--- code/main.py
+++ code/main.py
@@ -11,17 +11,16 @@ parser.add_argument('-type',  default='SimCLR',
                     help='Select the pretraining type',
                     choices=['SimCLR', 'InPainting'])
 parser.add_argument('--dataset', default='stl10',
-                    help='dataset name(stl10, cifar10), \
+                    help='dataset name: stl10 or cifar10, \
                     if costum place it in the data folder')
 parser.add_argument('--arch', default='Resnet18',
-                    choices=['Resnet18', 'Resnet18'], help='achitecture type')
+                    choices=['Resnet18', 'Resnet34'], help='achitecture type')
 
 parser.add_argument('--workers', default=6, type=int,
                     help='number of data loading workers for each loader')
 parser.add_argument('--epochs', default=200, type=int,
                     help='number of total epochs to run')
 parser.add_argument('--batch-size', default=128, type=int,
-                    metavar='N',
                     help='mini-batch size (default: 128), this is the total')
 
 parser.add_argument('--opti', '--optimizer', default='SGD',
@@ -29,7 +28,7 @@ parser.add_argument('--opti', '--optimizer', default='SGD',
                     choices=['SGD', 'Adam'])
 
 parser.add_argument('--lr', '--learning-rate', default=0.1, type=float,
-                    metavar='LR', help='initial learning rate', dest='lr')
+                    help='initial learning rate', dest='lr')
 parser.add_argument('--weight_decay', default=1e-4, type=float,
                     help='weight decay (default: 1e-4)')
 parser.add_argument('--momentum', default=0.9, type=float,
