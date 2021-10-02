9e6a0897431b045f2091c6569d50ecfca521988e
guy <guy5@post.bgu.ac.il>
2021-10-02 12:00:47 +0300

    Replaced SimCLR loss, fixed saved path

--- code/loss_functions.py
+++ code/loss_functions.py
@@ -1,6 +1,5 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 
 
 class SimClr_loss(nn.Module):
@@ -11,37 +10,27 @@ class SimClr_loss(nn.Module):
         self.n_views = args.n_views
         self.temperature = args.temperature
         self.device = device
-        self.criterion = torch.nn.CrossEntropyLoss()
 
     def forward(self, inputs):
         inputs = torch.cat(inputs, dim=0)
         _, features = self.net(inputs.to(self.device))
         batch_size = features.size(0) // self.n_views
 
-        labels = torch.cat([torch.arange(batch_size)
-                            for i in range(self.n_views)], dim=0)
-        labels = (labels.unsqueeze(0) == labels.unsqueeze(1))
-        labels = labels.to(self.device)
-
         sim_matrix = torch.matmul(features, features.T)
 
-        # discard the main diagonal from both: labels and similarities matrix
-        mask = ~torch.eye(labels.shape[0], dtype=torch.bool,
-                          device=self.device)
-        labels = labels[mask].view(labels.shape[0], -1)
-        sim_matrix = sim_matrix[mask].view(sim_matrix.shape[0], -1)
+        sim_ij = torch.diag(sim_matrix, batch_size)
+        sim_ji = torch.diag(sim_matrix, -batch_size)
+        positives = torch.cat([sim_ij, sim_ji], dim=0)
 
-        # select positives
-        positives = sim_matrix[labels].view(labels.shape[0], -1)
-        # select negatives
-        negatives = sim_matrix[~labels].view(sim_matrix.shape[0], -1)
+        negatives_mask = (~torch.eye(batch_size * 2, batch_size * 2,
+                                     dtype=bool, device=self.device)).float()
 
-        logits = torch.cat([positives, negatives], dim=1)
-        logits = logits / self.temperature
+        nominator = torch.exp(positives / self.temperature)
+        denominator = negatives_mask * torch.exp(sim_matrix / self.temperature)
 
-        labels = torch.zeros(logits.shape[0], dtype=torch.long,
-                             device=self.device)
-        return self.criterion(logits, labels)
+        loss_partial = -torch.log(nominator / torch.sum(denominator, dim=1))
+        loss = torch.sum(loss_partial) / (2 * batch_size)
+        return loss
 
 
 class InPainting_Loss(nn.Module):
